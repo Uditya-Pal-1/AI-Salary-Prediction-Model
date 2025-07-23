29560f69be99cab580063f2b8065bd3798174e2c
Uditya Pal <149376004+Aman-Pal-1@users.noreply.github.com>
2025-07-23 14:03:22 +0530

    change bg color white to blue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100 text-gray-800">
+    <div className="flex flex-col min-h-screen bg-blue-300 text-gray-800">
       <header className="bg-blue-600 text-white text-center py-6 shadow-lg">
         <h1 className="text-3xl font-bold">Employee Salary Predictor</h1>
         <p className="text-sm mt-1">Predict salaries based on your profile</p>
